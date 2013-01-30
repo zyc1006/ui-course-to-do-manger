9334e1709b6224a8c75091ecd6a3f219b3edc51f
zyc1006 <zyc1006@711a06a8-1512-35dc-8a6d-212254c5b4c3>
2013-01-30 21:51:48 +0000

    Initial to-do manager project

---
+++ trunk/ ui-course-to-do-manger --username zyc1006@gmail.com/ToDoManager/src/se/uu/it/todomanger/ui/MainWindow.java
@@ -0,0 +1,45 @@
+/**
+ * A test main window, will be modified later
+ */
+package se.uu.it.todomanger.ui;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ * @author Edward
+ *
+ */
+public class MainWindow extends JFrame {
+
+	//ContentPane
+	Container container = null;
+	
+	//Default constructor
+	public MainWindow(){
+		container = this.getContentPane();
+		container.setLayout(new GridLayout());
+		JLabel label = new JLabel("This is a JLabel",JLabel.CENTER);
+		label.setFont(new Font("SANS", Font.PLAIN, 22));
+		//Add label to container
+		container.add(label);
+		setSize(500, 200);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+	}
+	
+	public static void main(String[] args){
+		MainWindow window = new MainWindow();
+		
+	}
+	
+
+}
