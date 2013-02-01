48a69273c864b68284f7e6910032775b1ffed325
zyc1006 <zyc1006@711a06a8-1512-35dc-8a6d-212254c5b4c3>
2013-02-01 11:20:59 +0000

    Add a sketch class of TaskManager and Task

---
+++ trunk/ ui-course-to-do-manger --username zyc1006@gmail.com/ToDoManager/src/se/uu/it/todomanger/controller/TaskManager.java
@@ -0,0 +1,34 @@
+/**
+ * @author Edward
+ */
+
+package se.uu.it.todomanger.controller;
+
+import javax.swing.JList;
+
+import se.uu.it.todomanger.model.Task;
+
+/**
+ * TaskManger used for creating, editing and deleting tasks.
+ * @author Edward
+ *
+ */
+public class TaskManager {
+	
+	
+	public void addTask(Task task){
+		
+	}
+	public void editTask(Task task){
+		
+	}
+	public void deleteTask(Task task){
+		
+	}
+	private JList sort(int flag){
+	
+		JList retList = new JList();
+		return retList;
+	}
+	
+}
