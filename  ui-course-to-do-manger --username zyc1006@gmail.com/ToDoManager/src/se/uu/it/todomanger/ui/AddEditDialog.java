4206de27cc6f831557e7c581474f12b378839de4
1@flaskegger.net <1@flaskegger.net@711a06a8-1512-35dc-8a6d-212254c5b4c3>
2013-02-04 18:59:21 +0000

    Created a file for handling the Add/Edit dialog.

---
+++  ui-course-to-do-manger --username zyc1006@gmail.com/ToDoManager/src/se/uu/it/todomanger/ui/AddEditDialog.java
@@ -0,0 +1,27 @@
+/* TBD by Bjoern and Sara */
+
+package se.uu.it.todomanger.ui;
+
+import javax.swing.JFrame;
+
+import se.uu.it.todomanger.model.Task;
+
+/** 
+ * @author bjorn
+ * @author sara
+ */
+public class AddEditDialog extends JFrame {
+	
+	private Task task;
+	
+	// Constructor for adding new item.
+	public AddEditDialog() {
+		throw new UnsupportedOperationException("Not implemented.");
+	}
+	
+	// Constructor for editing existing item.
+	public AddEditDialog(Task editTask) {
+		throw new UnsupportedOperationException("Not implemented.");
+	}
+	
+}
