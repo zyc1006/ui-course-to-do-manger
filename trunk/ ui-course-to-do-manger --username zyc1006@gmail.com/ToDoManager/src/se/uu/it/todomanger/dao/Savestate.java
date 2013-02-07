03b4de4b89d46f168a06b279bce7fd8ddde3c45e
1@flaskegger.net <1@flaskegger.net@711a06a8-1512-35dc-8a6d-212254c5b4c3>
2013-02-07 16:46:08 +0000

    Mattias:es Savestate

---
+++ trunk/ ui-course-to-do-manger --username zyc1006@gmail.com/ToDoManager/src/se/uu/it/todomanger/dao/Savestate.java
@@ -0,0 +1,249 @@
+
+package se.uu.it.todomanger.dao;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import se.uu.it.todomanger.model.*;
+
+public class Savestate
+{
+    // Attributes
+    Task []taskList;
+    Category []categoryList;
+    
+    // Date format for our save state
+    SimpleDateFormat simpleDateFormat =
+            new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    
+    // Constructor
+    public Savestate(String fileName)
+    {
+        try
+        {
+            // Open the save file
+            File saveFile = new File(fileName);
+            DocumentBuilderFactory dbFactory =
+                    DocumentBuilderFactory.newInstance();
+            DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dbBuilder.parse(saveFile);
+            
+            // Normalize the document
+            doc.getDocumentElement().normalize();
+            
+            // Create node lists for tasks and categories
+            NodeList taskNodes     = doc.getElementsByTagName("task");
+            NodeList categoryNodes = doc.getElementsByTagName("category");
+            
+            // Get the number of tasks and categories
+            int numberOfTasks      = taskNodes.getLength();
+            int numberOfCategories = categoryNodes.getLength();
+            
+            // Create the arrays for tasks and categories
+            taskList     = new Task[numberOfTasks];
+            categoryList = new Category[numberOfCategories];
+            
+            // Fill task list with tasks from the save file
+            for(int i = 0; i < numberOfTasks; i++)
+            {
+                Node taskNode = taskNodes.item(i);
+                
+                // Create task in task list
+                if(taskNode.getNodeType() == Node.ELEMENT_NODE)
+                {
+                    Element taskElement = (Element) taskNode;
+                    
+                    // Gather temporary information about the task
+                    int tmpId             = Integer.parseInt(
+                            taskElement.getAttribute("id"));
+                    String tmpTitle       = taskElement.
+                            getElementsByTagName("title").item(0).
+                            getTextContent();
+                    Date tmpDate = simpleDateFormat.parse(taskElement.
+                            getElementsByTagName("date").item(0).
+                            getTextContent());
+                    int tmpCategoryId     = Integer.parseInt(taskElement.
+                            getElementsByTagName("categoryId").item(0).
+                            getTextContent());
+                    String tmpDescription = taskElement.
+                            getElementsByTagName("description").item(0).
+                            getTextContent();
+                    int tmpPriority       = Integer.parseInt(taskElement.
+                            getElementsByTagName("priority").item(0).
+                            getTextContent());
+                    boolean tmpCompleted  = Boolean.parseBoolean(taskElement.
+                            getElementsByTagName("completed").item(0).
+                            getTextContent());
+                    
+                    // Insert task into task list
+                    taskList[i] = new Task(tmpId, tmpTitle, tmpDate,
+                            tmpCategoryId, tmpDescription, tmpPriority,
+                            tmpCompleted);
+                }
+            }
+            
+            // Fill the category list with categories from the save file
+            for(int i = 0; i < numberOfCategories; i++)
+            {
+                Node categoryNode = categoryNodes.item(i);
+                
+                // Create category in cateory list
+                if(categoryNode.getNodeType() == Node.ELEMENT_NODE)
+                {
+                    Element categoryElement = (Element) categoryNode;
+                    
+                    // Gather temporary information about the category
+                    int tmpId             = Integer.parseInt(
+                            categoryElement.getAttribute("id"));
+                    String tmpTitle       = categoryElement.
+                            getElementsByTagName("title").item(0).
+                            getTextContent();
+                    
+                    // Insert category into category list
+                    categoryList[i] = new Category(tmpId, tmpTitle);
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("There were errors reading the save file");
+        }
+    }
+    
+    /*
+    // Method to save a save state
+    public void save(String fileName, boolean quickSave)
+    {
+        try
+        {
+            // Just overwrite the existing file
+            if(quickSave)
+            {
+                DocumentBuilderFactory docFactory = DocumentBuilderFactory.
+                        newInstance();
+                DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+                Document doc = docBuilder.parse(fileName);
+                
+                // Get the root element
+                Node savestateNode = doc.getFirstChild();
+                
+                
+            }
+            
+            // Save as new file
+            else
+            {
+
+            }
+        }
+        catch (ParserConfigurationException pce){pce.printStackTrace();}
+        //catch (TransformerException tfe){tfe.printStackTrace();}
+        catch (IOException ioe){ioe.printStackTrace();}
+        catch (SAXException sae){sae.printStackTrace();}
+    }*/
+    
+    // Method to get a new task id
+    public int getNewTaskId()
+    {
+        int newTaskId = 0;
+        
+        if(taskList.length != 0)
+        {
+            // Go through the task list to find the highest id
+            for(int i = 0; i < taskList.length; i++)
+            {
+                int tmpTaskId = taskList[i].getTaskId();
+                
+                if(newTaskId < tmpTaskId)
+                {
+                    newTaskId = tmpTaskId;
+                }
+            }
+            
+            // Increment by 1 to get a new highest id
+            newTaskId++;
+        }
+        
+        return newTaskId;
+    }
+    
+    // Method to get a new category id
+    public int getNewCategoryId()
+    {
+        int newCategoryId = 0;
+        
+        if(categoryList.length != 0)
+        {
+            // Go through the task list to find the highest id
+            for(int i = 0; i < categoryList.length; i++)
+            {
+                int tmpCategoryId = categoryList[i].getCategoryId();
+                
+                if(newCategoryId < tmpCategoryId)
+                {
+                    newCategoryId = tmpCategoryId;
+                }
+            }
+            
+            // Increment by 1 to get a new highest id
+            newCategoryId++;
+        }
+        
+        return newCategoryId;
+    }
+    
+    // Method to get the category title of a category id
+    public String getCategoryTitle(int categoryId)
+    {
+        String categoryTitle = "";
+        
+        for(int i = 0; i < categoryList.length; i++)
+        {
+            if(categoryList[i].getCategoryId() == categoryId)
+            {
+                categoryTitle = categoryList[i].getCategoryTitle();
+                
+                break;
+            }
+        }
+        
+        return categoryTitle;
+    }
+    
+    // Returns the task list
+    public Task [] getTaskList()
+    {
+        return taskList;
+    }
+    
+    // Returns the cateogyr list
+    public Category [] getCategoryList()
+    {
+        return categoryList;
+    }
+    
+    // Method to print all the unfinished tasks in the save state
+    public void printTasks()
+    {
+        for(int i = 0; i<taskList.length; i++)
+        {
+            if(!taskList[i].isCompleted())
+            {
+                System.out.println(getCategoryTitle(taskList[i].
+                        getTaskCategoryId()) + " - " + taskList[i].
+                        getTaskTitle() + "\n" + simpleDateFormat.
+                        format(taskList[i].getTaskDate()) + "\n");
+            }
+        }
+    }
+}
