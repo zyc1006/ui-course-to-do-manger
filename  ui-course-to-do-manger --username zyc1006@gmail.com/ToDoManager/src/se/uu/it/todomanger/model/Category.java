756c64d730cf5c53388312946373dc70894c6efb
1@flaskegger.net <1@flaskegger.net@711a06a8-1512-35dc-8a6d-212254c5b4c3>
2013-02-07 18:20:51 +0000

    Mattias:es Category class

---
+++  ui-course-to-do-manger --username zyc1006@gmail.com/ToDoManager/src/se/uu/it/todomanger/model/Category.java
@@ -0,0 +1,47 @@
+package se.uu.it.todomanger.model;
+
+public class Category
+{
+    // Attributes
+    private int id;
+    private String title;
+    private Category parent;
+    
+    // Constructor
+    public Category(int id, String title)
+    {
+        this.id    = id;
+        this.title = title;
+    }
+    // Constructor with parent
+    public Category(int id, String title, Category parent)
+    {
+        this.id     = id;
+        this.title  = title;
+        this.parent = parent;
+    }
+    
+    // Getters
+    public int getCategoryId()
+    {
+        return id;
+    }
+    public String getCategoryTitle()
+    {
+        return title;
+    }
+    public Category getCategoryParent()
+    {
+        return parent;
+    }
+    
+    // Setters
+    public void setCategoryTitle(String title)
+    {
+        this.title = title;
+    }
+    public void setCategoryParent(Category parent)
+    {
+        this.parent = parent;
+    }
+}
