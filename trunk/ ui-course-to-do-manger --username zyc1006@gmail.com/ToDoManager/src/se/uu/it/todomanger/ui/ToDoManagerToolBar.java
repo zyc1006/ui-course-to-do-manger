7211e5ea3659cb03a0e487ba5a43e1fab0c3e60d
1@flaskegger.net <1@flaskegger.net@711a06a8-1512-35dc-8a6d-212254c5b4c3>
2013-02-15 14:13:35 +0000

    Toolbar is now international.

--- trunk/ ui-course-to-do-manger --username zyc1006@gmail.com/ToDoManager/src/se/uu/it/todomanger/ui/ToDoManagerToolBar.java
+++ trunk/ ui-course-to-do-manger --username zyc1006@gmail.com/ToDoManager/src/se/uu/it/todomanger/ui/ToDoManagerToolBar.java
@@ -4,118 +4,141 @@ import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JToolBar;
 
 import se.uu.it.todomanger.controller.TaskManager;
+
 /**
- *  A singleton of tool bar for ToDoManager
+ * A singleton of tool bar for ToDoManager
+ * 
  * @author Shiyu
  * @author Yucheng
- *
+ * 
  */
 public class ToDoManagerToolBar extends JToolBar {
-	
-	
-	// Window  data
+
+	// Window data
 	public static final int TOOLBAR_BUTTON_WIDTH = 35;
 	public static final int TOOLBAR_BUTTON_HEIGHT = 35;
 	public static final int TOOLBAR_BUTTON_MAX_WIDTH = 35;
 	public static final int TOOLBAR_BUTTON_MAX_HEIGHT = 35;
-	
-	
-	// Language data
-	public static final String TOOLBAR_ADD_TASK = "Create task";
-	public static final String TOOLBAR_EDIT_TASK = "Edit task";
-	public static final String TOOLBAR_DELETE_TASK = "Delete task";
-	
+
 	// Icon file data
 	public static final String TOOLBAR_ICON_ADD_TASK = "res/image/Add.gif";
 	public static final String TOOLBAR_ICON_EDIT_TASK = "res/image/Edit.gif";
 	public static final String TOOLBAR_ICON_DELETE_TASK = "res/image/Delete.gif";
-	
-	
+
 	private static ToDoManagerToolBar toolBar = null;
 	
-	private ToDoManagerToolBar(){
+	// A ResourceBundle for handling internationalization
+	private static ResourceBundle resLocale;
+
+	private ToDoManagerToolBar() {
 		super();
 	}
-	
-	private static void initToolBar(){
-		
+
+	/**
+	 * initToolBar
+	 * 
+	 * @author Shiyu
+	 * @author Yucheng
+	 * @author Bjorn
+	 * 
+	 * @description Sets up the tool bar with icons and internationalized
+	 *              words/phrases.
+	 */
+	private static void initToolBar() {
+
+		// Load the international words/phrases from the resourcebundle.
+		try {
+			resLocale = ResourceBundle.getBundle("locale.ToDoManager",
+					Locale.getDefault());
+		} catch (MissingResourceException mre) {
+			System.err.println("res/locale/ToDoManager.properties not found");
+			System.exit(1);
+		}
+
 		ImageIcon imageIcon = new ImageIcon(TOOLBAR_ICON_ADD_TASK);
-		JButton createTaskButton = new JButton(scaleImage(imageIcon, TOOLBAR_BUTTON_WIDTH, TOOLBAR_BUTTON_HEIGHT));
-		createTaskButton.setToolTipText(TOOLBAR_ADD_TASK);
-		createTaskButton.setMaximumSize(new Dimension(TOOLBAR_BUTTON_MAX_WIDTH, TOOLBAR_BUTTON_MAX_HEIGHT));
+		JButton createTaskButton = new JButton(scaleImage(imageIcon,
+				TOOLBAR_BUTTON_WIDTH, TOOLBAR_BUTTON_HEIGHT));
+		createTaskButton.setToolTipText(resLocale.getString("MenuBar_Add_Task_Option"));
+		createTaskButton.setMaximumSize(new Dimension(TOOLBAR_BUTTON_MAX_WIDTH,
+				TOOLBAR_BUTTON_MAX_HEIGHT));
 		createTaskButton.setBorderPainted(false);
-		
+
 		imageIcon = new ImageIcon(TOOLBAR_ICON_EDIT_TASK);
-		JButton editTaskButton = new JButton(scaleImage(imageIcon, TOOLBAR_BUTTON_WIDTH, TOOLBAR_BUTTON_HEIGHT));
-		editTaskButton.setToolTipText(TOOLBAR_EDIT_TASK);
-		editTaskButton.setMaximumSize(new Dimension(TOOLBAR_BUTTON_MAX_WIDTH, TOOLBAR_BUTTON_MAX_HEIGHT));
+		JButton editTaskButton = new JButton(scaleImage(imageIcon,
+				TOOLBAR_BUTTON_WIDTH, TOOLBAR_BUTTON_HEIGHT));
+		editTaskButton.setToolTipText(resLocale.getString("MenuBar_Edit_Task_Option"));
+		editTaskButton.setMaximumSize(new Dimension(TOOLBAR_BUTTON_MAX_WIDTH,
+				TOOLBAR_BUTTON_MAX_HEIGHT));
 		editTaskButton.setBorderPainted(false);
-		
+
 		imageIcon = new ImageIcon(TOOLBAR_ICON_DELETE_TASK);
-		JButton delTaskButton = new JButton(scaleImage(imageIcon, TOOLBAR_BUTTON_WIDTH, TOOLBAR_BUTTON_HEIGHT));
-		delTaskButton.setToolTipText(TOOLBAR_DELETE_TASK);
-		delTaskButton.setMaximumSize(new Dimension(TOOLBAR_BUTTON_MAX_WIDTH, TOOLBAR_BUTTON_MAX_HEIGHT));
+		JButton delTaskButton = new JButton(scaleImage(imageIcon,
+				TOOLBAR_BUTTON_WIDTH, TOOLBAR_BUTTON_HEIGHT));
+		delTaskButton.setToolTipText(resLocale.getString("MenuBar_Delete_Task_Option"));
+		delTaskButton.setMaximumSize(new Dimension(TOOLBAR_BUTTON_MAX_WIDTH,
+				TOOLBAR_BUTTON_MAX_HEIGHT));
 		delTaskButton.setBorderPainted(false);
-		
+
 		toolBar.add(createTaskButton);
 		toolBar.addSeparator();
 		toolBar.add(editTaskButton);
 		toolBar.addSeparator();
 		toolBar.add(delTaskButton);
-		
-		//Actions for the buttons 
+
+		// Actions for the buttons
 		createTaskButton.addActionListener(new ActionListener() {
-			
-			//Open a dialog for adding tasks
+
+			// Open a dialog for adding tasks
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				AddEditDialog addEditDialog = new AddEditDialog();
 				addEditDialog.ShowAddDialog();
-				if(addEditDialog.clickedOK()){
-					//Add a task here
+				if (addEditDialog.clickedOK()) {
+					// Add a task here
 					TaskManager tm = TaskManager.getInstance();
 					tm.addTask(addEditDialog.getTask());
 					tm.displayTaskByDueDateAsc();
-				}
-				else {
+				} else {
 					System.out.println("cancel");
 				}
-				
-				
+
 			}
 		});
-		
+
 		editTaskButton.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				TaskManager.getInstance().displayTaskByDueDateDesc();
 			}
 		});
-		
+
 	}
-	
-	public static ToDoManagerToolBar getInstance(){
-		if(null == toolBar){
+
+	public static ToDoManagerToolBar getInstance() {
+		if (null == toolBar) {
 			toolBar = new ToDoManagerToolBar();
 			initToolBar();
 		}
 		return toolBar;
 	}
-	
-	private static ImageIcon scaleImage(ImageIcon icon, int width, int height){
+
+	private static ImageIcon scaleImage(ImageIcon icon, int width, int height) {
 		Image image = icon.getImage();
-		Image smallImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+		Image smallImage = image.getScaledInstance(width, height,
+				Image.SCALE_SMOOTH);
 		return new ImageIcon(smallImage);
-			
+
 	}
-	
-	
+
 }
