ba5f719f6d0eac7a132427f47be56fcc6b015cf3
zyc1006 <zyc1006@711a06a8-1512-35dc-8a6d-212254c5b4c3>
2013-03-08 20:54:51 +0000

    Replace JTable with JList in the widget. Implement a ListCellRenderer.

---
+++  ui-course-to-do-manger --username zyc1006@gmail.com/ToDoManager/src/se/uu/it/todomanger/ui/TaskListRenderer.java
@@ -0,0 +1,33 @@
+package se.uu.it.todomanger.ui;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class TaskListRenderer extends JLabel implements ListCellRenderer {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Component getListCellRendererComponent(JList list, Object value,
+			int index, boolean isSelected, boolean cellHasFocus) {
+		String s = value.toString();
+		setText(s);
+		if (isSelected) {
+			setBackground(new Color(43,97,238));
+			
+			setForeground(Color.white);
+		}
+		else{
+			setBackground(new Color(123, 211, 244));
+			setForeground(Color.white);
+		}
+
+		
+		
+		setEnabled(list.isEnabled());
+		setFont(list.getFont());
+		setOpaque(true);
+		return this;
+	}
+}
